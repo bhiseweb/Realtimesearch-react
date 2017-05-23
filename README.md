84fd69fa71bc13ebc9c6279bb31a10e0a4a6d401
Leena <leena.web11@gmail.com>
2017-05-23 17:20:00 +0530

    Basic real time search

---
+++ README.md
@@ -0,0 +1,2 @@
+# real-time-search
+This is a basic app in react which is performing real time search on client level.
---
+++ client/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RealTimeSearch from './components/real-time-search.jsx';
+
+ReactDOM.render(<RealTimeSearch />, document.getElementById('root'));
